e261d5234f6db62b232a9e2dc57198e75e4eaf49
Joran Honig <joran.honig@gmail.com>
2020-12-14 14:41:14 +0100

    add initial exercise files

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const VulnerableToken = artifacts.require("VulnerableToken");
 
 module.exports = deployer => {
-  deployer.deploy(Migrations);
+    deployer.deploy(Migrations);
+    deployer.deploy(VulnerableToken);
 };
